ceaac3180623a6e52767a17e0d85b12029a25d8e
LuizFhellippe <fhellippe04092001@gmail.com>
2021-08-16 14:36:40 -0300

    Chess exception and chess position

---
+++ Chess/ChessPosition.java
@@ -0,0 +1,35 @@
+package Chess;
+
+import boardGame.Position;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+    //metodo que pega a position do xadrex real
+    public ChessPosition(char column, int row){
+        if(column<'a' || column>'h' || row<1 || row>8){
+            throw new ChessException("Error instatialing chess position");
+        }
+        this.column = column;
+        this.row = row;
+    }
+    public char getColumn(){
+        return column;
+    }
+    public int getRow(){
+        return row;
+    }
+    //retorna a posicao como matriz
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+    //retorna a posicao da matriz como posicao normal
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition( (char)('a' - position.getcolumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString(){
+        return "" + column + row;
+    }
+}
