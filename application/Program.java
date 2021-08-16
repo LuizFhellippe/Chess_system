69a529492092eae8ad2f49484161622ea6e2877c
LuizFhellippe <fhellippe04092001@gmail.com>
2021-08-16 07:03:20 -0300

    Initial commit

---
+++ application/Program.java
@@ -0,0 +1,11 @@
+package Chess_system.application;
+
+import Chess_system.boardGame.Board;
+import Chess_system.boardGame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Board board = new Board(8, 8);
+    }
+}
---
+++ boardGame/Piece.java
@@ -0,0 +1,14 @@
+package Chess_system.boardGame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+    public Piece(Board board){
+        this.board = board;
+        position = null;
+    }
+    protected Board getBoard(){
+        return board;
+    }
+    //apenas o get, pq o set permite alterar o tabuleiro
+}
