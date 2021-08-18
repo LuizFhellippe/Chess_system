73cc9f2f0a84f69b58295e9c347009bdc665da0a
LuizFhellippe <fhellippe04092001@gmail.com>
2021-08-18 06:59:42 -0300

    Checkmate logic

--- Chess/ChessMatch.java
+++ Chess/ChessMatch.java
@@ -17,6 +17,7 @@ public class ChessMatch {
     private List<Piece> piecesOnTheBoard = new ArrayList<>();
     private List<Piece> capturedPieces = new ArrayList<>();
     private boolean check;
+    private boolean checkMate;
     public ChessMatch(){
         board = new Board(8, 8);
         turn = 1;
@@ -31,6 +32,9 @@ public class ChessMatch {
     }
     public boolean getCheck(){
         return check;
+    }
+    public boolean getCheckMate(){
+        return checkMate;
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -60,7 +64,12 @@ public class ChessMatch {
 
         check = (testCheck(opponent(currentPlayer))) ? true : false;
 
-        nextTurn();
+        if(testCheckMate(opponent(currentPlayer))){
+            checkMate = true;
+        }else{
+            nextTurn();
+        }
+
         return (ChessPiece)capturedPiece;
     }
     private Piece makeMove(Position source, Position target){
@@ -128,6 +137,30 @@ public class ChessMatch {
             }
         }
         return false;
+    }
+    private boolean testCheckMate(Color color){
+        if(!testCheck(color)){
+            return false;
+        }
+        List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+        for(Piece p : list){
+            boolean[][] mat = p.possibleMoves();
+            for(int i=0; i<board.getRows(); i++){
+                for(int j=0; j<board.getColumns(); j++){
+                    if(mat[i][j]){
+                        Position source = ((ChessPiece)p).getChessPosition().toPosition();
+                        Position target = new Position(i, j);
+                        Piece capturedPiece = makeMove(source, target);
+                        boolean testCheck = testCheck(color);
+                        undoMove(source, target, capturedPiece);
+                        if(!testCheck){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
     //coloca a peca passando as coordenadas na posicao de xadrez
     private void placeNewPiece(char column, int row, ChessPiece piece){
